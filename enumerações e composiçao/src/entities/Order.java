7fe700d2593e783b9dd6265b9e997dc814b95798
Henry Adam <iihenymii@gmail.com>
2022-04-18 20:53:42 -0300

    Teste git

---
+++ enumerações e composiçao/src/entities/Order.java
@@ -0,0 +1,88 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	private static SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+
+	private Date moment;
+	private OrderStatus status;
+	
+	private Client client;
+	private List<OrderItem> items = new ArrayList<>();
+	
+	public Order() {
+	}
+
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+
+	public void addItem(OrderItem item) {
+		items.add(item);
+	}
+	
+	public void removeItem(OrderItem item) {
+		items.remove(item);
+	}
+	
+	public double total() {
+		double sum = 0;
+		for (OrderItem o : items) {
+			sum += o.subtotal();
+		}
+		return sum;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("ORDER SUMMARY:\n");
+		sb.append("Order moment: ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append("Order status: " + status + "\n");
+		sb.append("Client: " + client.getName() + " " + "(" + sdf1.format(client.getBirthDate()) + ")" + " - " + client.getEmail() + "\n");
+		sb.append("Order items:\n");
+		for (OrderItem o : items) {
+			sb.append(o.getProduct().getName() + ", $" + o.getProduct().getPrice() + ", Quantity: " + o.getQuantity() + ", Subtotal: $" + o.subtotal() + "\n");
+		}
+		sb.append("Total price: $" + String.format("%.2f", total()));
+		return sb.toString();
+	}
+}
